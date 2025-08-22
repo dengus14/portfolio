03945bfa72d96e695c0411ab67ace3505433fe89
dengus14 <ddgusev0318@gmail.com>
2025-08-22 10:18:46 -0500

    health point

--- server/index.js
+++ server/index.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const app = express();
-const port = 5000;
+const port = 4000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+app.get('/api/health', (req, res) => {
+  res.json({ status: 'ok' });
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at ${port}`)
