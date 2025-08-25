69ab6bdf2e87593c51d18a827e48fbaceee80c01
dengus14 <ddgusev0318@gmail.com>
2025-08-25 10:41:38 -0500

    connecting mongoDB

--- server/index.js
+++ server/index.js
@@ -5,6 +5,9 @@ const express = require('express')
 const cors = require('cors');
 const app = express();
 const port = 4000;
+const connectDB = require('./config/db');
+
+connectDB()
 
 app.use(cors());
 
