865182183b212a681c4c03b52a33020e66527ba9
dengus14 <ddgusev0318@gmail.com>
2026-03-24 16:19:09 -0500

    fixes

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,3 @@
-import { useEffect } from 'react';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
 import React from 'react';
 import NavBar from './components/NavBar/NavBar';
 import Home from './components/Home/Home';
--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Footer.css";
-import { FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 const Footer = () => {
   return (
